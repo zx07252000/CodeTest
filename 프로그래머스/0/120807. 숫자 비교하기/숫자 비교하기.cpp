1df55f213395361818ba073c2e82509ba6d68655
zx07252000 <43131637+zx07252000@users.noreply.github.com>
2024-11-15 14:31:41 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    
+    // Upload Test
+    
+    answer = num1 == num2 ? 1 : -1;
+    
+    return answer;
+}
