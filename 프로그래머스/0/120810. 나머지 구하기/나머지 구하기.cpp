f998c2cf741fcef29dbb7c578c2a56b5b308587d
zx07252000 <43131637+zx07252000@users.noreply.github.com>
2024-11-18 14:20:47 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    
+    // Upload Test
+    
+    answer = num1%num2;
+    
+    return answer;
+}
